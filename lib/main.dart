69fb1c537d3731b379373fcbc27254918936a90a
sassank <sassan.khalafi@gmail.com>
2024-05-02 22:42:47 +0200

    update Welcome Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'constants.dart';
 
 void main() => runApp(const MyApp());
 
-//this widget is the root of your application
+// This widget is the root of your application
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -21,40 +20,79 @@ class MyApp extends StatelessWidget {
   }
 }
 
-  //welcome screen
-  class WelcomeScreen extends StatelessWidget{
+// Welcome screen
+class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Column(
-          children: <Widget>[
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
           Expanded(
             flex: 3,
             child: Container(
               decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.white12,
+                    Colors.black,
+                  ],
+                ),
                 image: DecorationImage(
                   image: AssetImage('assets/gym.jpg'),
-                  fit: BoxFit.fitWidth,
+                  fit: BoxFit.fitHeight,
+                  opacity: 0.3,
+                ),
+              ),
+              // Add the RichText widget containing "Go Coach" text here
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,// Center vertically
+                  children: <Widget>[
+                    RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Go Coach",
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 60,
+                              fontWeight: FontWeight.w400,
+                              shadows: [ // Add shadows here!
+                                Shadow(
+                                  offset: Offset(4.0, 4.0), // Adjust offset for shadow position
+                                  blurRadius: 50.0, // Adjust blurRadius for shadow spread
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Divider( // HR (horizontal rule) line
+                      height: 40,
+                      thickness: 0.5,
+                      indent: 90,
+                      endIndent: 90,
+                      color: Colors.white70, // Adjust color as needed
+                    ),
+                    const Text(
+                      "Ce qui importe, c'est le progrès, \n non la perfection.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
           ),
-          Expanded(
-            child: Column(
-              children: <Widget>[
-                RichText(
-                  text: const TextSpan(
-                    children: [TextSpan(text: "Go Coach")]
-                    ),
-                  ),
-              ]
-            ),
-          )
-          ]
-        ),
-      );
-    }
+        ],
+      ),
+    );
   }
-
+}
